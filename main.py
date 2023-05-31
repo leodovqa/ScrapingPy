e529d8760a35b3434ca98e425c8b9059f449fc5b
leodovqa <leoqatester@gmail.com>
2023-06-01 00:21:24 +0300

    fixed but need to do another one on the get image src

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import re
 from bs4 import BeautifulSoup
 import requests
 import os
@@ -18,27 +19,37 @@ data = response.text
 soup = BeautifulSoup(data, 'html.parser')
 infoBoxs = soup.find_all("li", {"class": "s-item s-item--large"})
 count = 0
-f = open("result.txt", "a", encoding='utf-8')
+# f = open("result.txt", "a", encoding='utf-8')
 res = []
 for infoBox in infoBoxs:
     title = infoBox.find("h3", {"class": "s-item__title"}).text
     price = infoBox.find("span", {"class": "s-item__price"}).text
     href = infoBox.find("a", {"class": "s-item__link"}).get("href")
     img = infoBox.find("img", {"class": "s-item__image-img"})
-    test = infoBox.find(img, {"img": "data-src"})
-    print(img)
-    print(test)
+    img = str(img).replace("<", "").replace(">", "")
+    if "data-src=" in img:
+        s2 = "data-src="
+        s3 = img[img.index(s2) + len(s2):]
+        s3 = img.split('data-src=', 1)
+        s3 = s3[1].split(' src=', 1)
+        res = s3[0]
+    else:
+        s2 = "src="
+        s3 = img[img.index(s2) + len(s2):]
+        s3 = s3[:-1]
+        res = s3
     if "to" in price:
         price = price.split('to', 1)[0]
     count += 1
     title = title.replace("✅", " ").replace("❖", "").replace("✤", "")
+    '''
     result = str(
         count) + "." + "\n" + "Title: " + title + "\n" + "Price: " + price + "\n" + "Link: " + href + "\n" + "Image Source: " + \
-             img[
-                 "src"] + "\n" + liner
-    json_result = "Title: " + title, "Price: " + price, "Link: " + href
-
-    f.write(result + "\n")
+             str(res) + "\n" + liner
+    '''
+    json_result = "Title: " + title, "Price: " + price, "Link: " + href, "Image Source: " + str(res)
+    print(str(res))
+    # f.write(result + "\n")
     res.append(json_result)
 
 with open('results.json', 'w', encoding='utf-8') as f:
