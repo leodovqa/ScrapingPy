255990d16721247a6e05d9c65885c82d583b65fc
leodovqa <leoqatester@gmail.com>
2023-06-04 22:24:19 +0300

    fixed but need to do another one on the get image src

--- test_file.py
+++ test_file.py
@@ -1,7 +1,18 @@
-s1 = '<img alt="Samsung Galaxy A73 5G 128GB+8GB RAM Dual Sim Unlocked New-Multi color" class="s-item__image-img" data-src="https://i.ebayimg.com/thumbs/images/g/r6sAAOSwwS9kHfkF/s-l300.jpg" src="https://ir.ebaystatic.com/cr/v/c1/s_1x2.gif"/>'
-s2 = "data-src="
-x2 = "src="
-s3 = s1[s1.index(s2) + len(s2):]
-s4 = s3[s3.index(x2) + len(x2):]
-s3 = s3.split(' src=', 1)
-print(s3[0])
+import json
+import sys
+
+# load the data into an element
+with open('results.json') as user_file:
+    file_contents = user_file.read()
+
+# dumps the json object into an element
+json_str = json.dumps(file_contents)
+
+# load the json to a string
+resp = json.loads(json_str)
+
+# print the resp
+print(resp)
+
+# extract an element in the response
+
